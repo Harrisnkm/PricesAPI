f78866e147e07d19bce237765a9a37e02a4a4610
vagrant <vagrant@homestead>
2020-07-04 13:42:47 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+        colors: {
+            'brand-blue': '1992d4',
+        },
+        spacing: {
+            '72': '18 rem'
+        }
+    },
+  },
+  variants: {
+      backgroundColor: ['responsive', 'hover', 'focus', 'active']
+  },
+  plugins: [],
+}
